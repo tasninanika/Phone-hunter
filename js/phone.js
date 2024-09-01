d1d554f8b382b63c8047d3f5da45c21520af5e7f
anikatasnin <jarintasnin09@gmail.com>
2024-09-01 00:04:18 +0000

    card added

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,38 @@
-console.log('hunting');
+const loadPhone = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json();
+    const phones = data.data;
+    console.log(phones);
+    displayPhones(phones);
+}
+
+const displayPhones = phones => {
+    // console.log(phones);
+    // 1. get container
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone =>{
+        console.log(phone);
+        // 2. create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
+        // 3. inner html
+        phoneCard.innerHTML = `
+        <figure>
+            <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        // 4. apend child
+        phoneContainer.appendChild(phoneCard);
+    } )
+}
+
+loadPhone();
