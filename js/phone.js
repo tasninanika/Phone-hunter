14e32c8fcb32d0a53af7a144a990a2a47eb102d8
anikatasnin <jarintasnin09@gmail.com>
2024-09-01 00:11:06 +0000

    card updated

--- js/phone.js
+++ js/phone.js
@@ -19,11 +19,11 @@ const displayPhones = phones => {
         phoneCard.innerHTML = `
         <figure>
             <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src="${phone.image}"
             alt="Shoes" />
         </figure>
         <div class="card-body">
-            <h2 class="card-title">Shoes!</h2>
+            <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
